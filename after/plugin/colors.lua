f91f9d3fcc8f98ccdc37215bedcd87e278ea6cc1
Rootiens <rootiens@gmail.com>
2023-08-25 10:42:24 +0330

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,7 @@
 function ColorMyNVIM(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    -- color = color or "rose-pine"
+    color = color or "dark_flat"
+    vim.cmd.colorscheme(color)
 end
 
 ColorMyNVIM()
