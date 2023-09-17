28f9aa5351fd37bcc0bbf54fdba7734d4ff18f55
rootiens <rootiens@gmail.com>
2023-09-17 17:55:37 +0330

    autopairs

--- lua/rootiens/packer.lua
+++ lua/rootiens/packer.lua
@@ -29,20 +29,20 @@ return require('packer').startup(function(use)
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
@@ -52,7 +52,7 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
-    use {"sekke276/dark_flat.nvim"}
+    use { "sekke276/dark_flat.nvim" }
     use 'AlexvZyl/nordic.nvim'
     use {
         'nvim-lualine/lualine.nvim',
@@ -62,4 +62,8 @@ return require('packer').startup(function(use)
     use 'folke/tokyonight.nvim'
     use 'ray-x/go.nvim'
     use 'ray-x/guihua.lua'
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
